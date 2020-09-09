c5730b9187ec77c16d2b40d9251c731000be611f
Rafael Santana <rafaelsantana7213@gmail.com>
2020-09-09 19:19:16 -0300

    Página de 404

---
+++ src/pages/Error/index.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css';
+import './styles.css';
+import Lottie from 'react-lottie';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import NotFound from '../../lotties/404.json';
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: NotFound,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice"
+  }
+};
+
+function Error() {
+  useEffect(() => {
+    AOS.init({
+      duration: 3000
+    })
+  }, []);
+
+  return (
+    <div>
+      <header data-aos="zoom-out-up" className='center-align notFoundBlock'>
+        <div className="col s12 m12 l12">
+
+          <Lottie className='404'
+            options={defaultOptions}
+          />
+          <h2>PÁGINA NÃO ENCONTRADA</h2>
+
+        </div>
+      </header>
+
+    </div>
+  )
+}
+
+
+export default Error;
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import Header from './components/Header'
 import Footer from './components/Footer';
 import About from './pages/About';
 import Games from './pages/Games';
+import Error from './pages/Error';
 
 const Routes = ()=>{
  return(
@@ -15,6 +16,7 @@ const Routes = ()=>{
    <Route exact path="/" component={Home}/>
    <Route exact path="/sobre" component={About}/>
    <Route exact path="/jogos" component={Games}/>
+   <Route path="*" component={Error}/>
   </Switch>
   <Footer/>
   </BrowserRouter>
